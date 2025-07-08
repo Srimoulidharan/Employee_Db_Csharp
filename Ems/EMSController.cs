be166abf0d15d9568c32c3361bb04739c652dada
Srimoulidharan <141481080+Srimoulidharan@users.noreply.github.com>
2025-07-08 19:58:09 +0530

    Add files via upload

---
+++ Ems/EMSController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Ems
+{
+    public class EMSController
+    {
+        private MySqlConnection connection;
+
+        public EMSController()
+        {
+            String connect = "server=localhost;Database=Ems;Uid=root;password=977327341426";
+            connection = new MySqlConnection(connect);
+        }
+        public EMS AddEmployee(EMS emp)
+        {
+            connection.Open();
+
+            String query = "Insert into employee(Name, Salary, Department) values(@name,@salary,@dept)";
+            var cmd=new MySqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@name", emp.Name);
+            cmd.Parameters.AddWithValue("@salary",emp.Salary);
+            cmd.Parameters.AddWithValue("@dept",emp.Department);
+            int res=cmd.ExecuteNonQuery();
+            connection.Close();
+            if (res > 0)
+            {
+                Console.WriteLine("Added");
+                return emp;
+           
+            }
+            else
+            {
+                Console.WriteLine("Not Added");
+                return null;
+               
+            }
+
+
+        }
+        public bool UpdateEmployee(EMS emp)
+        {
+            connection.Open();
+            string query = "UPDATE employee SET Name = @name, Salary = @salary, Department = @dept WHERE Id = @id";
+            var cmd = new MySqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@id",emp.Id);
+            cmd.Parameters.AddWithValue("@name", emp.Name);
+            cmd.Parameters.AddWithValue("@salary", emp.Salary);
+            cmd.Parameters.AddWithValue("@dept", emp.Department);
+            int res = cmd.ExecuteNonQuery();
+            connection.Close();
+            if (res > 0)
+            {
+                Console.WriteLine("Employee Updated");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Not Updated");
+                return false;
+            }
+
+        }
+        public void DeleteEmployee(EMS emp) {
+            connection.Open();
+
+            string query = "Delete from employee WHERE Id = @id";
+            var cmd = new MySqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@id", emp.Id);
+            int res = cmd.ExecuteNonQuery();
+            connection.Close();
+            if (res > 0)
+            {
+                Console.WriteLine("Employee Deleted");
+            }
+            else
+            {
+                Console.WriteLine("Not Added");
+                
+
+            }
+        }
+
+        public List<EMS> viewAll() 
+        {
+            connection.Open();
+            string query = "select * from employee";
+            List<EMS> list = new List<EMS>();
+            var cmd = new MySqlCommand(query, connection);
+            var reader= cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                var EMS = new EMS
+                {
+                    Id = reader.GetInt32("id"),
+                    Name= reader.GetString("name"),
+                    Salary=reader.GetString("salary"),
+                    Department=reader.GetString("department")
+                };
+                list.Add(EMS);  
+
+            }
+            reader.Close();
+            connection.Close();
+
+            return list;
+
+        }
+
+        public EMS ViewOne(int id) {
+            connection.Open();
+            string query = "select * from employee where Id=@id";
+            var cmd = new MySqlCommand(query, connection);
+            EMS empl = null;
+            cmd.Parameters.AddWithValue("@id",id);
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                empl = new EMS
+                {
+                    Id = reader.GetInt32("id"),
+                    Name = reader.GetString("name"),
+                    Salary = reader.GetString("salary"),
+                    Department = reader.GetString("department")
+                };
+            }
+            reader.Close();
+            connection.Close();
+            return empl;
+        }
+    }
+}
---
+++ Ems/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EMSController controller = new EMSController();
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("1. Add Employee");
+                Console.WriteLine("2. Update Employee");
+                Console.WriteLine("3. Delete Employee");
+                Console.WriteLine("4. View All Employees");
+                Console.WriteLine("5. View One Employee");
+                Console.WriteLine("6. Exit");
+                Console.Write("Choose an option: ");
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        AddEmployee(controller);
+                        break;
+                    case "2":
+                        UpdateEmployee(controller);
+                        break;
+                    case "3":
+                        DeleteEmployee(controller);
+                        break;
+                    case "4":
+                        ViewAllEmployees(controller);
+                        break;
+                    case "5":
+                        ViewOneEmployee(controller);
+                        break;
+                    case "6":
+                        exit = true;
+                        Console.WriteLine("Exiting...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+            }
+        }
+
+        static void AddEmployee(EMSController controller)
+        {
+            EMS emp = new EMS();
+
+            Console.Write("Enter Name: ");
+            emp.Name = Console.ReadLine();
+
+            Console.Write("Enter Salary: ");
+            emp.Salary = Console.ReadLine();
+
+            Console.Write("Enter Department: ");
+            emp.Department = Console.ReadLine();
+
+            controller.AddEmployee(emp);
+        }
+
+        static void UpdateEmployee(EMSController controller)
+        {
+            EMS emp = new EMS();
+
+            Console.Write("Enter Employee ID to Update: ");
+            emp.Id = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter New Name: ");
+            emp.Name = Console.ReadLine();
+
+            Console.Write("Enter New Salary: ");
+            emp.Salary = Console.ReadLine();
+
+            Console.Write("Enter New Department: ");
+            emp.Department = Console.ReadLine();
+
+            controller.UpdateEmployee(emp);
+        }
+
+        static void DeleteEmployee(EMSController controller)
+        {
+            EMS emp = new EMS();
+
+            Console.Write("Enter Employee ID to Delete: ");
+            emp.Id = int.Parse(Console.ReadLine());
+
+            controller.DeleteEmployee(emp);
+        }
+
+        static void ViewAllEmployees(EMSController controller)
+        {
+            List<EMS> employees = controller.viewAll();
+
+            Console.WriteLine("Employees");
+            foreach (var emp in employees)
+            {
+                Console.WriteLine($"ID: {emp.Id}, Name: {emp.Name}, Salary: {emp.Salary}, Department: {emp.Department}");
+            }
+        }
+
+        static void ViewOneEmployee(EMSController controller)
+        {
+            Console.Write("Enter Employee ID to View: ");
+            int id = int.Parse(Console.ReadLine());
+
+            EMS emp = controller.ViewOne(id);
+
+            if (emp != null)
+            {
+                Console.WriteLine($"ID: {emp.Id}, Name: {emp.Name}, Salary: {emp.Salary}, Department: {emp.Department}");
+            }
+            else
+            {
+                Console.WriteLine("Employee not found.");
+            }
+        }
+    }
+}
